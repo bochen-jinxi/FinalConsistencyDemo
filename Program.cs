43ba2926747332499d4f3490b98c3f3f43cf78dd
chenbo <2984294503@qq.com>
2025-05-21 10:23:08 +0800

    s

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Hosting;
 namespace FinalConsistencyDemo
 {
     public class Program
-    {
+    { 
         public static void Main(string[] args)
             => CreateHostBuilder(args).Build().Run();
 
